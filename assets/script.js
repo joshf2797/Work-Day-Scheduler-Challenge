116758f303c72d63a77a094a3b3a2ba93336e57a
Joshua <joshf2797@gmail.com>
2022-12-13 21:38:40 -0700

    polished project

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+var currentHour = dayjs().hour();
+var currentTime = dayjs()
+
+
+$(document).ready(function () {
+  $('.saveBtn').on('click', function () {
+
+    var textArea = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+  
+  localStorage.setItem(time, textArea);
+  })
+});
+
+function timeBlockUpdate() {
+
+  $(".time-block").each(function () {
+    var timeInBlock = parseInt($(this).attr('id').split('-')[1]);
+
+    if(timeInBlock < currentHour) {
+      $(this).addClass('past');
+    } else if (timeInBlock === currentHour) {
+      $(this).removeClass('past');
+      $(this).addClass('present');
+    }else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  });
+}
+
+timeBlockUpdate();
+
+setInterval(timeBlockUpdate, 10000);
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+
+
+var currentDay = dayjs().format('dddd, MMMM D, YYYY');
+$("#currentDay").text(currentDay);
